c35f44a266002fdca29dd75e19477237d1291f92
Ibrahim <mohammed.ibrahim.datasci@gmail.com>
2026-06-22 20:48:36 +0530

    New Problem

--- DSA-IN-C++/00-Basics/logic-building/03-Basic-Math/26-Prime-Check.cpp
+++ DSA-IN-C++/00-Basics/logic-building/03-Basic-Math/26-Prime-Check.cpp
@@ -1,10 +1,56 @@
-#include<iostream>
+/*
+Problem: Check whether a number is prime or not.
+
+Approach:
+- A prime number has exactly two factors: 1 and itself.
+- Numbers less than or equal to 1 are not prime.
+- Check divisibility from 2 up to the square root of the number.
+- If any divisor is found, the number is not prime.
+- Otherwise, the number is prime.
+
+Time Complexity: O(√n)
+Space Complexity: O(1)
+
+Example:
+Input: 13
+Output: A prime number
+
+Input: 12
+Output: Not a prime number
+*/
+
+#include <iostream>
 using namespace std;
-int main ()
+
+int main()
 {
-    int prime;
-    cin >> prime;
+    int number;
+    cin >> number;
 
-    if()
+    // 0 and 1 are not prime numbers
+    if(number <= 1)
+    {
+        cout << "Not a prime number" << endl;
+        return 0;
+    }
 
+    bool prime = true;
+
+    // Check divisors only up to sqrt(number)
+    for(int i = 2; i * i <= number; ++i)
+    {
+        // If divisible, it is not prime
+        if(number % i == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+
+    if(prime)
+        cout << "A prime number" << endl;
+    else
+        cout << "Not a prime number" << endl;
+
+    return 0;
 }
