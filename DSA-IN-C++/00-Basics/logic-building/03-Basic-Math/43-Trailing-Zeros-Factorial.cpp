23e21969a0d5fee949b866cb4dda683863b4bd60
Ibrahim <mohammed.ibrahim.datasci@gmail.com>
2026-07-15 23:09:39 +0530

    Add Trailing Zeros in Factorial solution

---
+++ DSA-IN-C++/00-Basics/logic-building/03-Basic-Math/43-Trailing-Zeros-Factorial.cpp
@@ -0,0 +1,54 @@
+/*
+Problem: Count Trailing Zeros in Factorial
+
+Topic:
+- Math
+- Number Theory
+
+Pattern:
+- Mathematical Observation
+
+Approach:
+- Trailing zeros are created by multiplying 2 and 5.
+- Since factors of 2 are always more than factors of 5, count only the number of factors of 5.
+- Count multiples of 5, 25, 125, ... and add their contribution.
+- Continue until the power of 5 becomes greater than n.
+
+Time Complexity: O(log₅ n)
+Space Complexity: O(1)
+
+Example:
+Input:
+n = 100
+
+Calculation:
+100 / 5 = 20
+100 / 25 = 4
+
+Total trailing zeros = 24
+
+Output:
+24
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    long long trailingZeros = 0;
+    long long powerOfFive = 5;
+
+    while (powerOfFive <= n)
+    {
+        trailingZeros += n / powerOfFive;
+        powerOfFive *= 5;
+    }
+
+    cout << trailingZeros << '\n';
+
+    return 0;
+}
